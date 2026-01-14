a44df7cf4c8662b72b9d9432b30c83836611a737
Christian Camilo Serna Gamez <christian.sernag@bam.com.gt>
2026-01-14 08:05:25 -0500

    se agrega RateLimit general

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,9 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id?:$request->ip());
-        });
+        
         /*RateLimiter::for('global', function (Request $request) {
             return Limit::perMinute(1000)->by($request->ip());
         });*/
