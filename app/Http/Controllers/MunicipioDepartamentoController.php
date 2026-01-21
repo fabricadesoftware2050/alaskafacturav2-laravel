25d70c810c9de0ceb166f210d86eb3b36959a1e8
Christian Camilo Serna Gamez <christian.sernag@bam.com.gt>
2026-01-21 17:21:26 -0500

    se agrega caché para no usar conexiones a BD

--- app/Http/Controllers/MunicipioDepartamentoController.php
+++ app/Http/Controllers/MunicipioDepartamentoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Municipio;
+use Illuminate\Support\Facades\Cache;
 
 
 class MunicipioDepartamentoController extends Controller
@@ -13,6 +14,7 @@ class MunicipioDepartamentoController extends Controller
      */
     public function index()
     {
+        /*
         try {
             $municipios = Municipio::with('departamento')
                 ->join('departamentos', 'departamentos.id_departamento', '=', 'municipios.departamento_id')
@@ -30,7 +32,41 @@ class MunicipioDepartamentoController extends Controller
 
         } catch (\Exception $e) {
             return response()->json(['success' => false, 'message' => 'Error al listar municipios ' . $e->getMessage()], 500);
-        }
+        }*/
+
+    try {
+
+        $resultado = Cache::rememberForever('municipios_departamentos', function () {
+
+            return Municipio::join(
+                    'departamentos',
+                    'departamentos.id_departamento',
+                    '=',
+                    'municipios.departamento_id'
+                )
+                ->orderBy('departamentos.departamento')
+                ->orderBy('municipios.municipio')
+                ->selectRaw("
+                    UPPER(municipios.municipio) as municipio,
+                    UPPER(departamentos.departamento) as departamento
+                ")
+                ->get()
+                ->map(function ($row) {
+                    return "{$row->municipio}, {$row->departamento}";
+                });
+
+        });
+
+        return response()->json(['municipios' => $resultado]);
+
+    } catch (\Exception $e) {
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Error al listar municipios'
+        ], 500);
+    }
+
     }
 
     /**
